78022c8956c501e5a111ed2522edb1fe2b99d7c0
tk0987 <109526789+tk0987@users.noreply.github.com>
2025-02-20 22:09:37 +0100

    wifi support ok

--- main_machine.py
+++ main_machine.py
@@ -1,17 +1,11 @@
-# this code was written for raspberry pi pico 
-# i do not want to rewrite sth like grbl, so my pc does everything
-# smooth enough
-
 import sys
-# import network
-# import socket
+import network
+import socket
 from utime import sleep, ticks_ms,ticks_diff
 from machine import Pin,PWM,freq
 import select
 import math
 machine.freq(125000000)
-# import network
-# import socket #those later
 
 global steps_per_revolution
 steps_per_revolution=200
@@ -40,9 +34,52 @@ pinsy = [IN1y, IN2y]
 
 pinsz = [IN1z, IN2z]
  
+global pulse_x
+global pulse_y
+global pulse_z
+pulse_x=0
+pulse_y=0
+pulse_z=0
+
+# Set up Pico W as an access point
+ap = network.WLAN(network.AP_IF)
+ap.active(True)
+ap.config(essid='picoCNC', password='picowcnc')
+
+
+
+# Set up server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('0.0.0.0', 8000))
+server.listen(1)
+
+
+def x_callback(pin):
+#     if pin == IN1x:
+    global pulse_x
+    pulse_x+=1
+    return pulse_x
+
+def y_callback(pin):
+#     if pin == IN1y:
+    global pulse_y
+    pulse_y+=1
+    return pulse_y
+
+def z_callback(pin):
+#     if pin == IN1z:
+    global pulse_z
+    pulse_z+=1
+    return pulse_z
 
 def freq_set(x_step,y_step,z_step):
-    length=abs(x_step)+abs(y_step)+abs(z_step) # now normalised correctly...
+    global pulse_x
+    global pulse_y
+    global pulse_z
+    pulse_x=0
+    pulse_y=0
+    pulse_z=0
+    length=math.sqrt((x_step)**2+(y_step)**2+(z_step)**2)
     if x_step!=0:
         rel_speed_factor_x=abs(x_step)/length
     else:
@@ -55,10 +92,8 @@ def freq_set(x_step,y_step,z_step):
         rel_speed_factor_z=abs(z_step)/length
     else:
         rel_speed_factor_z=0
-   
- 
-    base_freq=1000 # i have weak engines - 5 rpm is even too much
- # what's more - this code is working, but engines can move faster. note - if, let's say, x=y and z=0, then x&y move with 500 Hz freq. and they can move with 1000 Hz each other
+    speed_factor=rel_speed_factor_x+rel_speed_factor_y+rel_speed_factor_z
+    base_freq=800
     if x_step!=0:
         freq_x=int(base_freq*(rel_speed_factor_x))
     else:
@@ -71,11 +106,17 @@ def freq_set(x_step,y_step,z_step):
         freq_z=int(base_freq*(rel_speed_factor_z))
     else:
         freq_z=0
-
+    # time_needed=int(max(abs(x_step),abs(y_step),abs(z_step))/max(freq_x,freq_y,freq_z))*1000000
+    # start_time = ticks_us()
+    pins = [
+        (IN1x, freq_x,2*abs(x_step)),   
+        (IN1y, freq_y,2*abs(y_step)),   
+        (IN1z, freq_z,2*abs(z_step)),  
+    ]
     pin_states = [False] * len(pins)
     last_toggle_times = [0] * len(pins)
     oscillation_counts = [0] * len(pins)
-    while True: # this loop, with help of chatgpt 3.5, moves engines semi-asynchronously. they have diffrent frequencies (as pathlengths are different), but they need to finish their work together (for gcode).
+    while True:
         current_time = ticks_ms()
 
         for i, (pin, frequency, max_oscillations) in enumerate(pins):
@@ -89,8 +130,12 @@ def freq_set(x_step,y_step,z_step):
             break
 
 
+    
+
+
+
 def move(x_step,y_step,z_step):
-#global current_position_x      #those 'current positions' will be used when i will have money for absolute coordinates. you know - all of those encoders and 'krańcówki'. and time (time is money).
+#global current_position_x
 #global current_position_y
 #global current_position_z
     global pinsx
@@ -112,24 +157,43 @@ def move(x_step,y_step,z_step):
     if z_step<0:
         pinsz[1].value(0)
 
-    sleep(0.0005) # work cannot wait
+    #control numbers for step count
+    # control_x=0
+    # control_y=0
+    # control_z=0
+    sleep(0.001)
     freq_set(x_step,y_step,z_step)
- 
+
 
 while True:
-    if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
-        a = sys.stdin.readline().strip()
-        if len(a) > 0:
-            led.value(1)
-            command = a
-#             if len(command) <= 1:
-#                 reset_position()
-#                 print("ok")
-#             else:
-            parts = command.split(" ")# single space - pc app uses three spaces. it is 'todo'. niby małe g(...), ale upierdliwe przy edycji
-            x = int(parts[0])
-            y = int(parts[1])
-            z = int(parts[2])
-            move(x_step=x, y_step=y, z_step=z)
-            print("ok") # for the pc app
-            led.value(0)
+    client, address = server.accept()
+    data = client.recv(1024)
+    if data:
+        # print('Received:', data.decode())
+        led.value(1)
+        parts = data.split(" ")
+        x = int(parts[0])
+        y = int(parts[1])
+        z = int(parts[2])
+        move(x_step=x, y_step=y, z_step=z)
+        client.sendall(b'Ok')
+    client.close()
+
+
+#     if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:   # this is for usb only
+#         a = sys.stdin.readline().strip()
+#         if len(a) > 0:
+#             led.value(1)
+#             command = a
+# #             if len(command) <= 1:
+# #                 reset_position()
+# #                 print("ok")
+# #             else:
+#             parts = command.split(" ")
+#             x = int(parts[0])
+#             y = int(parts[1])
+#             z = int(parts[2])
+#             move(x_step=x, y_step=y, z_step=z)
+#             print("ok")
+#             led.value(0)
+
